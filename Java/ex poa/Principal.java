e842199d46cf262021cc78333ce9fbcedbd466ca
CesarRFernandes <crf1513@gmail.com>
2025-08-24 19:05:55 -0300

    agenda

--- Java/ex poa/Principal.java
+++ Java/ex poa/Principal.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Principal {
+    public static void main(String[] args){
+        Agenda a1 = new Agenda();
+        a1.escrita("César","123");
+        a1.lerTodos();
+        a1.ler("César");
+    }
 }
