f49c5d61d4f5e6b47fb4808507858b263abad932
Caio dos Santos Fusco <140856552+caiofusco@users.noreply.github.com>
2025-08-27 20:51:10 -0300

    Ajuste

--- Java/ex poa/agenda/Principal.java
+++ Java/ex poa/agenda/Principal.java
@@ -2,6 +2,7 @@ public class Principal {
     public static void main(String[] args){
         Agenda a1 = new Agenda();
         a1.escrita("César","123");
+        a1.escrita("Caio","523");
         a1.lerTodos();
         a1.ler("César");
     }
